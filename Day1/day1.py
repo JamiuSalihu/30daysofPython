d3d4fe945e521b8004d3ed78d4350d390c41aea4
Jamiusaliu <salihumuhammadjamiu19@gmail.com>
2024-02-13 14:33:44 +0100

    day1.py

---
+++ Day1/day1.py
@@ -0,0 +1,26 @@
+print('Hello Ict')
+a = 20
+print(type(a))
+
+# to print a string
+print('Hello mom!')
+
+# to print integer, float
+print(int(30))
+print(float(300))
+
+# to represent 15 in binary number, octal and hexa
+a = 15
+print(bin(a))
+print(oct(a))
+print(hex(a))
+print(bin(23))
+print(oct(8))
+
+# reusing
+
+x,y =1,1
+print(x is y)
+
+a,b = 258, 258
+print(a is b)
